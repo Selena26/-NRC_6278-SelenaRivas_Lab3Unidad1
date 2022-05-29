8108d96cb03f30ffd06b10cef289a4efc27d1543
Selena26 <75510255+Selena26@users.noreply.github.com>
2022-05-29 11:57:05 -0500

    Importar libreria y creación de la clase grafo para implem,entar un algoritmo de búsqueda por profundidad

---
+++ SR_DFS.py
@@ -0,0 +1,10 @@
+from queue import Queue
+'''Importa la libreria Queue'''
+class Grafo:
+    '''Clase grafo-Representa a un grafo aplicando búsqueda en profundidad
+    Atributos:
+    numero_de_nodos:número de nodos
+    dirigido:si es dirigido o no dirigido
+    Excepciones:
+    Evalua --Si (__name__ == "__main__")
+    '''
