8ed0f11beada3825581f00c8fc9c76149563cbaa
Selena26 <75510255+Selena26@users.noreply.github.com>
2022-05-29 12:11:57 -0500

    Método __init__ para inicializar el contructor de la clase grafo y uso de docstrings para descripción

--- SR_DFS.py
+++ SR_DFS.py
@@ -8,3 +8,15 @@ class Grafo:
     Excepciones:
     Evalua --Si (__name__ == "__main__")
     '''
+    def __init__(self, numero_de_nodos, dirigido = True):
+        '''Método constructor inicializador de la clase Grafo
+        Parámetros:
+        m_numero_de_nodos--- el número de nodos
+        m_nodos--- rango de nodos
+        m_dirigido--- dirigido o no dirigido (por defecto en verdadero). Dato de tipo boleano
+        m_lista_adyacencia--- Lista de adyacencia (Uso de un diccionario para implementar una lista de adyacencia)
+        '''
+        self.m_numero_de_nodos = numero_de_nodos
+        self.m_nodos = range(self.m_numero_de_nodos)
+        self.m_dirigido = dirigido
+        self.m_lista_adyacencia = {nodo: set() for nodo in self.m_nodos}
